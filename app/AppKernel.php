dd24bcb038d4be33fd88ae9d856aeb0f7f15f637
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-07-05 02:10:32 +0200

    Instalado DoctrineFixtures

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new IMDRest\DirectorBundle\DirectorBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
         );
 
