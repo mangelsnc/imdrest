2469a2ad95b99ccf772e6abc8667ca4b9fa2386e
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-07-05 02:11:54 +0200

    Incorporada la clase utils

---
+++ src/IMDRest/PeliculaBundle/Util/Util.php
@@ -0,0 +1,15 @@
+<?php
+namespace IMDRest\PeliculaBundle\Util;
+
+class Util
+{
+    public static function getSlug($cadena, $separador = '-')
+    {
+        $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $cadena);
+        $slug = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $slug);
+        $slug = strtolower(trim($slug, $separador));
+        $slug = preg_replace("/[\/_|+ -]+/", $separador, $slug);
+
+        return $slug;
+    }
+}
