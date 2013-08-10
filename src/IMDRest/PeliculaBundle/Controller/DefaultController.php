ce81a2f0a0fed703adc56b445bf13a85f5a7d194
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2013-08-10 20:10:08 +0200

    Añadidas las anotaciones de ruta y eliminado el controlador por defecto

--- src/IMDRest/PeliculaBundle/Controller/DefaultController.php
+++ src/IMDRest/PeliculaBundle/Controller/DefaultController.php
@@ -2,12 +2,10 @@
 
 namespace IMDRest\PeliculaBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('PeliculaBundle:Default:index.html.twig', array('name' => $name));
-    }
+    
 }
